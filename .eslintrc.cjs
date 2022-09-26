35f4d57377e7d0a8632d663b22ffe349a3e9ee80
claire lin <clairelin@kklab.com>
2022-09-26 16:56:33 +0800

    Add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: [
+        'src/**/*.ts',
+        'src/**/*.tsx',
+      ],
+      extends: [
+        'airbnb-typescript/base',
+        'plugin:import/recommended',
+      ],
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+        ],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+     '@typescript-eslint'
+  ],
+  rules: {
+  },
+};
